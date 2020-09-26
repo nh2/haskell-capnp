f5304e6c33ebdfb2752bec38e8f80ed7b6e005ea
Ian Denhardt <ian@zenhack.net>
2020-09-26 12:30:48 -0400

    Add some comments to Capnp.Mutability.

--- lib/Capnp/Mutability.hs
+++ lib/Capnp/Mutability.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeFamilies               #-}
+-- | Utility module for dealing with mutable & immutable variants of a type.
 module Capnp.Mutability
     ( Mutability(..)
     , MaybeMutable(..)
@@ -11,8 +12,16 @@ import Control.Monad.Primitive (PrimMonad(PrimState))
 
 import Data.Kind (Type)
 
+-- | An indication of mutability; this is intended to be used with the
+--
+-- @DataKinds@ language extension and types of kind @f :: 'Mutability' -> *@,
+-- where @f 'Const@ is an immutable version of the type, and @f ('Mut s)@ is
+-- a mutable version of the type which can be mutated in an instance of
+-- 'PrimMonad' with state token @s@.
 data Mutability = Mut Type | Const
 
+-- | An instance of 'MaybeMutable' allows conversion between mutabale
+-- and immutable forms of a type.
 class MaybeMutable (f :: Mutability -> *) where
     -- | Convert an immutable value to a mutable one.
     thaw :: (PrimMonad m, PrimState m ~ s) => f 'Const -> m (f ('Mut s))
@@ -35,7 +44,14 @@ class MaybeMutable (f :: Mutability -> *) where
     unsafeFreeze :: (PrimMonad m, PrimState m ~ s) => f ('Mut s) -> m (f 'Const)
     unsafeFreeze = freeze
 
-newtype Phantom a (mut :: Mutability) = Phantom { unPhantom :: a }
+-- | A phantom affords using a type without separate mutable & immutable
+-- variants in a place where somthing of kind @'Mutability' -> *@ is
+-- expected; @'Phantom' a mut@ is a trivial wrapper around 'a'.
+--
+-- It has an instance of 'MaybeMutable' whose thaw & freeze operations are
+-- no-ops.
+newtype Phantom a (mut :: Mutability)
+    = Phantom { unPhantom :: a }
     deriving(Num)
 
 instance MaybeMutable (Phantom a) where
